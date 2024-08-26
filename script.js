4cd7fe6f96426563fdf5eabf79b5acea9b2bb4e7
Nitishprasadverma <nitishverma773964@gmail.com>
2024-08-26 16:36:20 +0530

    further proceed project day2

---
+++ script.js
@@ -0,0 +1,34 @@
+
+
+
+// page-2-Animation
+var elements = document.querySelectorAll(".right-elem");
+
+elements.forEach(function(elem){
+    elem.addEventListener("mouseenter", function(){
+       
+       
+       
+        gsap.to(elem.childNodes[3],{
+        opacity:1,
+        scale:1
+       })
+    })
+
+    elem.addEventListener("mouseleave", function(){
+        gsap.to(elem.childNodes[3],{
+            opacity:0,
+            scale:0
+           })
+    })
+
+    elem.addEventListener("mousemove",function(dets){
+        console.log(elem.getBoundingClientRect());
+
+        gsap.to(elem.childNodes[3],{
+            x:dets.x - elem.getBoundingClientRect().x -90,
+            y:dets.y - elem.getBoundingClientRect().y - 215
+            
+        })
+    })
+})
