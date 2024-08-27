f8fc7ba34c3b661b1b27d498d8fe2a2eeb221fef
Nitishprasadverma <nitishverma773964@gmail.com>
2024-08-27 06:03:01 +0530

    hello

--- script.js
+++ script.js
@@ -2,33 +2,63 @@
 
 
 // page-2-Animation
-var elements = document.querySelectorAll(".right-elem");
 
-elements.forEach(function(elem){
-    elem.addEventListener("mouseenter", function(){
-       
-       
-       
-        gsap.to(elem.childNodes[3],{
-        opacity:1,
-        scale:1
-       })
-    })
+function page2Animation(){
+    var elements = document.querySelectorAll(".right-elem");
 
-    elem.addEventListener("mouseleave", function(){
-        gsap.to(elem.childNodes[3],{
-            opacity:0,
-            scale:0
+    elements.forEach(function(elem){
+        elem.addEventListener("mouseenter", function(){
+           
+           
+           
+            gsap.to(elem.childNodes[3],{
+            opacity:1,
+            scale:1
            })
-    })
-
-    elem.addEventListener("mousemove",function(dets){
-        console.log(elem.getBoundingClientRect());
-
-        gsap.to(elem.childNodes[3],{
-            x:dets.x - elem.getBoundingClientRect().x -50,
-            y:dets.y - elem.getBoundingClientRect().y - 120
-            
+        })
+    
+        elem.addEventListener("mouseleave", function(){
+            gsap.to(elem.childNodes[3],{
+                opacity:0,
+                scale:0
+               })
+        })
+    
+        elem.addEventListener("mousemove",function(dets){
+            console.log(elem.getBoundingClientRect());
+    
+            gsap.to(elem.childNodes[3],{
+                x:dets.x - elem.getBoundingClientRect().x -50,
+                y:dets.y - elem.getBoundingClientRect().y - 120
+                
+            })
         })
     })
-})
+    
+}
+
+function page3Animation(){
+    var page3Center = document.querySelector(".page3-center")
+    var video = document.querySelector("#page3 video")
+    
+    page3Center.addEventListener("click", function () {
+        video.play()
+        gsap.to(video, {
+            transform: "scaleX(1) scaleY(1)",
+            opacity: 1,
+            borderRadius: 0
+        })
+    })
+    video.addEventListener("click", function () {
+        video.pause()
+        gsap.to(video, {
+            transform: "scaleX(0.7) scaleY(0)",
+            opacity: 0,
+            borderRadius: "30px"
+        })
+    })
+}
+
+
+page2Animation();
+page3Animation();
