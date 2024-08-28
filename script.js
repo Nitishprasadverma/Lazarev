13debcaf690ac148c98128b7756862628945c772
Nitishprasadverma <nitishverma773964@gmail.com>
2024-08-28 23:09:33 +0530

    day4

--- script.js
+++ script.js
@@ -57,6 +57,21 @@ function page3Animation(){
             borderRadius: "30px"
         })
     })
+
+    var sections = document.querySelectorAll(".section-right")
+
+    sections.forEach(function(elem) {
+        elem.addEventListener("mouseenter", function () {
+            elem.childNodes[3].style.opacity = 1
+            elem.childNodes[3].play()
+            
+        })
+
+        elem.addEventListener("mouseleave", function(){
+            elem.childNodes[3].style.opacity = 0;
+            elem.childNodes[3].load()
+        })
+    })
 }
 
 
