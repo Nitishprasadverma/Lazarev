0c98ab61a7dcae522aefed8e2cb6c68147c0da82
Nitishprasadverma <nitishverma773964@gmail.com>
2024-08-31 10:44:29 +0530

    loading animation and locomotive-scroll animation

--- script.js
+++ script.js
@@ -1,5 +1,84 @@
 
+// locomotive
+function locomotiveAnimation(){
+    gsap.registerPlugin(ScrollTrigger);
 
+    // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+    
+    const locoScroll = new LocomotiveScroll({
+      el: document.querySelector("#main"),
+      smooth: true
+    });
+    // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+    locoScroll.on("scroll", ScrollTrigger.update);
+    
+    // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+    ScrollTrigger.scrollerProxy("#main", {
+      scrollTop(value) {
+        return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+      }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+      getBoundingClientRect() {
+        return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+      },
+      // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+      pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+    });
+    
+    
+  
+    
+    
+    
+   
+    // --- PURPLE/GREEN PANEL ---
+    var tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: ".purple",
+          scroller: "#main",
+          scrub: true,
+          pin: true,
+          start: "top top",
+          end: "+=100%"
+        }
+      });
+    
+    
+    
+    
+    // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+    
+    // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+    ScrollTrigger.refresh();
+    
+}
+
+// loading
+function loadingAnimation(){
+    var t1 = gsap.timeline()
+    t1.from("#page1",{
+        opacity:0,
+        duration:0.2,
+        delay:0.2
+    })
+
+    t1.from("#page1",{
+        transform: "scaleX(0.7) scaleY(0.2) translateY(80%)",
+        borderRadius:"150px",
+        duration:1,
+        ease:"expo.out"
+    })
+
+    t1.from("#menu-bar",{
+        opacity:0,
+        delay:-0.2
+    })
+    t1.from("#page1 p, #page div",{
+        opacity:0,
+        duration:0.1,
+        stagger:0.2
+    })
+}
 
 // page-2-Animation
 
@@ -36,7 +115,7 @@ function page2Animation() {
     })
 
 }
-
+// page3
 function page3Animation() {
     var page3Center = document.querySelector(".page3-center")
     var video = document.querySelector("#page3 video")
@@ -112,7 +191,7 @@ function page6() {
     })
 }
 
-
+// page7
 function page7() {
     // part-2++++++++++++++++++++++++++++++++++
     gsap.from("#btm7-part2 h4 ", {
@@ -120,7 +199,7 @@ function page7() {
         duration: 1,
         scrollTrigger: {
             trigger: "#btm7-part2",
-            scroller: "body",
+            scroller: "#main",
             // markers:true
             start: "top 80%",
             end: "top 10%"
@@ -135,7 +214,7 @@ function page7() {
         duration: 1,
         scrollTrigger: {
             trigger: "#btm7-part3",
-            scroller: "body",
+            scroller: "#main",
             // markers:true
             start: "top 80%",
             end: "top 10%"
@@ -150,7 +229,7 @@ function page7() {
         duration: 1,
         scrollTrigger: {
             trigger: "#btm7-part4",
-            scroller: "body",
+            scroller: "#main",
             // markers:true
             start: "top 80%",
             end: "top 10%"
@@ -160,7 +239,8 @@ function page7() {
 
     )
 }
-
+loadingAnimation()
+locomotiveAnimation();
 page2Animation();
 page3Animation();
 page6();
